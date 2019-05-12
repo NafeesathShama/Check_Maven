e1eb65e6ff835d8e057f21ff621728c78bcb4764
Dell <Dell@WINCTRL-JQLLI9C>
2019-05-12 12:25:29 +0530

    going to commit

---
+++ src/p1/Check.java
@@ -0,0 +1,23 @@
+package p1;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class Check 
+{
+	@Test
+	public void test()
+	{
+		System.setProperty("webdriver.gecko.driver","./Software/geckodriver.exe");
+		WebDriver driver=new FirefoxDriver();
+		driver.get("https://www.flipkart.com/");
+		List<WebElement> links = driver.findElements(By.tagName("a"));
+		int count = links.size();
+		System.out.println(count);
+	}
+}
